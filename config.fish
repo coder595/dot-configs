a7e41190150e905993cd89ec8f32d96182d91a9a
coder595 <muhammad@github.com>
2024-02-12 21:28:43 +0400

    config.fish

--- config.fish
+++ config.fish
@@ -7,9 +7,36 @@
 
 set fish_greeting #Suppres the Welcome greeting.
 
-set auth 'doas'
+set auth 'sudo'
 
-alias ls='ls --color=auto'
+set browser 'floorp'
+
+#alias ls='ls --color=auto'
+
+alias office='flatpak run org.onlyoffice.desktopeditors'
+alias discord='flatpak run com.discordapp.Discord'
+
+alias quran='firefox-esr https://quran.com/'
+alias sunnah='firefox-esr https://sunnah.com/'
+alias bukhari='firefox-esr https://sunnah.com/bukhari'
+alias muslim='firefox-esr https://sunnah.com/muslim'
+
+alias gpt='thorium-browser https://chat.openai.com/'
+alias gemini='thorium-browser https://gemini.google.com/app'
+alias YT='thorium-browser https://www.youtube.com/'
+alias WA='thorium-browser https://web.whatsapp.com/'
+
+alias pmtr='$browser https://www.physicsandmathstutor.com/'
+alias chemr='$browser https://chemrevise.org/international-a-level/'
+alias pastr='$browser https://sites.google.com/view/paperlords2/ial-recent-papers'
+alias pastc='$browser https://sites.google.com/view/paperlords2/ial-recent-papers/ial-chemistry'
+alias pastp='$browser https://sites.google.com/view/paperlords2/ial-recent-papers/ial-physics'
+
+alias la='exa -a'
+alias ll='exa -al'
+alias ls='exa'
+
+
 alias fdisk='$auth fdisk -l'
 alias df='df -h'
 alias c='clear'
@@ -17,7 +44,7 @@ alias at='alacritty-themes'
 alias ..='cd ..'
 
 alias mkinstall='$auth make clean install'
-alias mkuninstall='$authmake clean uninstall'
+alias mkuninstall='$auth make clean uninstall'
 
 alias mount='$auth mount'
 alias umount='$auth umount'
@@ -40,11 +67,9 @@ alias rofi='rofi -show run'
 alias brightness='$auth brillo -q -A 5'
 alias brightless='$auth brillo -q -U 5'
 
-alias bio-class='chromium http://demo.polr.me/1'
-alias wapp='chromium https://web.whatsapp.com'
-
-colorscript random
-pfetch 
+ #colorscript random
+ #figlet ==MN7==
+ #pfetch
 
 # This file is run by all fish instances.
 # To include configuration only for login shells, use
@@ -55,4 +80,3 @@ pfetch
 # if status is-interactive
 #   ...
 # end
-#starship init fish | source
